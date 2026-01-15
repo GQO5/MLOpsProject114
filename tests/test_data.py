371f901eb8e8a8aeaed72be2a8dd15c78129546a
Burak Yenice <burak.ync8@gmail.com>
2026-01-15 13:08:58 +0100

    dataloader again

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,16 @@ from mlopsproject.data import load_data
 def test_load_data():
     """Test the load_data function."""
     try:
-        train_loader, val_loader, test_loader, train_raw, val_raw, test_raw, y_mean, y_std = load_data()
+        (
+            train_loader,
+            val_loader,
+            test_loader,
+            train_raw,
+            val_raw,
+            test_raw,
+            y_mean,
+            y_std,
+        ) = load_data()
         assert train_loader is not None
         assert val_loader is not None
         assert test_loader is not None
@@ -13,4 +22,5 @@ def test_load_data():
     except AssertionError:
         # If data files don't exist, skip the test
         import pytest
+
         pytest.skip("Data files not available for testing")
