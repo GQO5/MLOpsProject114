b2044902e4af1deef1cdbaaf6f63ba4b2c64b3ae
Tero <egomezcabello@gmail.com>
2026-01-22 09:12:49 +0100

    Add W&B API key and job_config. for Vertex AI

--- train.sh
+++ train.sh
@@ -6,10 +6,11 @@ git init
 # Pull data from DVC
 uv run dvc pull
 
-# Login to wandb if API key is provided
-if [ -n "$WANDB_API_KEY" ]; then
-    uv run wandb login $WANDB_API_KEY
-fi
+# Set W&B API key
+export WANDB_API_KEY=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
+
+# Login to wandb
+uv run wandb login $WANDB_API_KEY
 
 # Preprocess data
 uv run invoke preprocess-data
