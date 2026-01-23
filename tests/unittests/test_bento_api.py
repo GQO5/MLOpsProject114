b84366e3bbb1133c58aaa7552770de2b966498ce
Ivan Dimitrov <ivandm2509@gmail.com>
2026-01-23 11:03:06 +0100

    Skip api test in macOS

--- tests/unittests/test_bento_api.py
+++ tests/unittests/test_bento_api.py
@@ -1,10 +1,17 @@
 import io
+import subprocess
+import sys
 
+import bentoml
+import pytest
 import requests
 from PIL import Image
-import subprocess
-import bentoml
 
+
+@pytest.mark.skipif(
+    sys.platform == "darwin",
+    reason="Skipping test on macOS due to timeout issues while starting the BentoML client.",
+)
 def test_predict_endpoint():
     """
     Test the /predict endpoint of the BentoML API.
@@ -14,9 +21,19 @@ def test_predict_endpoint():
     - The response JSON contains the keys: total_calories, total_fat, total_carb, total_protein.
     - The values for these keys are floats.
     """
-    with subprocess.Popen(["bentoml", "serve", "src.mlopsproject.bento_backend.bentoml_service:ImageClassifierService", "--port", "5000"]) as proc:
+    with subprocess.Popen(
+        [
+            "bentoml",
+            "serve",
+            "src.mlopsproject.bento_backend.bentoml_service:ImageClassifierService",
+            "--port",
+            "5000",
+        ]
+    ) as proc:
         try:
-            client = bentoml.SyncHTTPClient("http://localhost:5000", server_ready_timeout=60)
+            client = bentoml.SyncHTTPClient(
+                "http://localhost:5000", server_ready_timeout=60
+            )
             # Create a dummy image in memory, and convert it to bytes
             img = Image.new("RGB", (224, 224), color="white")
             img_bytes = io.BytesIO()
@@ -27,10 +44,13 @@ def test_predict_endpoint():
 
             for key in ["total_calories", "total_fat", "total_carb", "total_protein"]:
                 assert key in response, f"Missing key in response: {key}"
-                assert isinstance(response[key], float), f"Expected float for {key}, got {type(response[key])}"
+                assert isinstance(
+                    response[key], float
+                ), f"Expected float for {key}, got {type(response[key])}"
         finally:
             proc.terminate()
             proc.wait()
+
 
 if __name__ == "__main__":
     test_predict_endpoint()
